5d58812fbfa833b53c8b91724c2cf2c97dcefa32
karthik-mallesh <3699karthik@gmail.com>
2025-05-08 14:28:48 +0530

    bugs fixing

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/chat/:id", protectRoute, getMessages);
+router.get("/:userId", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
 
